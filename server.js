96fa7ccc0e0a9291cb3b899355033febe8456b67
Maurizio Brioschi <maurizio.brioschi@limango.de>
2018-04-18 10:13:09 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+//middleware to log requests
+app.use((req, res, next)=>{
+  var now = new Date().toString();
+  fs.appendFile('server.log', `${now} ${req.method} ${req.url}\n`, (err) => {
+    if(err) {
+      console.log(error);
+    }
+  });
+  next();
+});
+
+app.use((req, res, next)=>{
+  res.render('maintenance.hbs');
+});
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (message) => {
+  return message.toUpperCase();
+});
+
+app.get('/',(request, response)=>{
+  response.send('Hello express');
+});
+
+app.get('/about', (req, res)=>{
+  res.render('about.hbs', {
+    text: 'troppo il migliore'
+  });
+});
+
+app.listen(3000, ()=> {
+  console.log('Server is up on port 3000');
+});
